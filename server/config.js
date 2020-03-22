1e221549ab57dbf510151f7fc2cd5fd2faa461d6
Prakash Dathu Parsa <parsadathu@gmail.com>
2020-03-22 20:04:50 +0530

    Added Authentication , email verification. Completed Error Handling for user model

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   mongoURL: process.env.SERVER_DB || 'mongodb+srv://i708563:T1gc6JzX2utzaFcE@cluster0-vlu1n.mongodb.net/hospitalDB?retryWrites=true&w=majority',
-  port: process.env.SERVER_PORT || 3030,
+  port: process.env.PORT || 3030,
   env: process.env.NODE_ENV || 'dev',
   jwt_secret: process.env.JWT_SECRET || 'JustDoIt',
 };
